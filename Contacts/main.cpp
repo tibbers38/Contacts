d2ae2d66500fc3f1fc16aaf40c16dc6d27a31c96
Nam Lê Hải <lehainam38@gmail.com>
2018-10-20 20:37:13 +0700

    Initial Commit

---
+++ Contacts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Contacts
+//
+//  Created by Nam 😋 on 10/20/18.
+//  Copyright © 2018 Nam 😋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
